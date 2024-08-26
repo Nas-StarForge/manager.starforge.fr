0c490dccf55326d7903d70bc75770ecce50825c5
Azarow <thibault.pointurier.pro@gmail.com>
2024-08-26 23:51:19 +0200

    chore: reformat code & add dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
-import { getDirname } from '@adonisjs/core/helpers'
-import inertia from '@adonisjs/inertia/client'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
+import inertia from '@adonisjs/inertia/client'
+import { getDirname } from '@adonisjs/core/helpers'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: false } }), vue(), adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    vue(),
+    adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] }),
+  ],
 
-  /**
-   * Define aliases for importing modules from
-   * your frontend code
-   */
   resolve: {
     alias: {
-      '~/': `${getDirname(import.meta.url)}/inertia/`,
+      '@/': `${resolve(getDirname(import.meta.url), 'inertia')}/`,
+      '~/': `${resolve(getDirname(import.meta.url), '.')}/`,
     },
   },
 })
