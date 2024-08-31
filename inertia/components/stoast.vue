1895275b71886021ec3f50eab805efa0f602037d
Azarow <thibault.pointurier.pro@gmail.com>
2024-08-31 22:31:55 +0200

    feat: add component toast

---
+++ inertia/components/stoast.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { watch } from 'vue'
+import { usePage } from '@inertiajs/vue3'
+import { useToast } from 'vue-toastification'
+import type { TYPE } from 'vue-toastification'
+
+type ToasterType = {
+  type: string
+  message: string
+}
+
+const toast = useToast()
+const page = usePage()
+
+watch(
+  () => page.props.toast,
+  (newVal: ToasterType) => {
+    toast(newVal.message, {
+      type: newVal.type as TYPE,
+    })
+  },
+)
+</script>
