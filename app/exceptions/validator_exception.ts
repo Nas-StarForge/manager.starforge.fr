dffff1f9adde52e17fdce4af9eaeaef0697085cf
Azarow <thibault.pointurier.pro@gmail.com>
2024-09-02 00:16:47 +0200

    chore: add type for error

--- app/exceptions/validator_exception.ts
+++ app/exceptions/validator_exception.ts
@@ -5,7 +5,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class ValidatorException extends ExceptionHandler {
   async handle(error: unknown, ctx: HttpContext) {
     if (error instanceof errors.E_VALIDATION_ERROR) {
-      error.messages.map((item) => {
+      error.messages.map((item: { detail: string }) => {
         ctx.session.flash('toast', {
           type: 'error',
           message: item.detail,
