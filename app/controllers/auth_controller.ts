a8fbb36a51734a79b8fefc2244cc3ef3d5d046e5
Azarow <thibault.pointurier.pro@gmail.com>
2024-09-03 00:06:45 +0200

    feat(auth): add flash message

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -13,14 +13,19 @@ export default class AuthController {
     return inertia.render('auth/register')
   }
 
-  async register({ request, response }: HttpContext) {
+  async register({ request, response, session }: HttpContext) {
     const data = await request.validateUsing(registerValidator)
     await User.create(data)
+
+    session.flash('toast', {
+      type: 'success',
+      message: 'Le compte a été créé avec succès !',
+    })
 
     return response.redirect().toRoute('auth.login')
   }
 
-  async login({ request, response }: HttpContext) {
+  async login({ request, response, session }: HttpContext) {
     const data = await request.validateUsing(loginValidator)
     const user = await User.verifyCredentials(data.email, data.password)
 
@@ -33,11 +38,21 @@ export default class AuthController {
       httpOnly: true,
     })
 
+    session.flash('toast', {
+      type: 'success',
+      message: 'Connexion effectuée !',
+    })
+
     return response.redirect().toRoute('profile')
   }
 
-  async logout({ response }: HttpContext) {
+  async logout({ response, session }: HttpContext) {
     response.clearCookie(String(env.get('AUTH_COOKIE_NAME')))
+
+    session.flash('toast', {
+      type: 'success',
+      message: 'Vous avez été déconnecté.',
+    })
     return response.redirect().toRoute('auth.login')
   }
 }
