5a231206d59eb46bdfaf1b499e3af6f996a47483
Azarow <thibault.pointurier.pro@gmail.com>
2024-09-07 05:52:53 +0200

    fix: add dashboard layouts

--- inertia/pages/dashboard/home.vue
+++ inertia/pages/dashboard/home.vue
@@ -1,5 +1,7 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Dashboard from '@/layouts/dashboard.vue'
+</script>
 
 <template>
-  <h1>test</h1>
+  <Dashboard><h1>test</h1> </Dashboard>
 </template>
