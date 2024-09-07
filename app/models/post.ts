80301e961794edf32b0281bdab72380b9ec75a04
Azarow <thibault.pointurier.pro@gmail.com>
2024-09-07 05:03:36 +0200

    feat: add post migration & model

---
+++ app/models/post.ts
@@ -0,0 +1,43 @@
+import { DateTime } from 'luxon'
+import { randomUUID } from 'node:crypto'
+import { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { BaseModel, beforeCreate, belongsTo, column } from '@adonisjs/lucid/orm'
+
+import User from '#models/user'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: string
+
+  @column()
+  declare title: string
+
+  @column()
+  declare content: string
+
+  @column()
+  declare imageUrl: string
+
+  @column()
+  declare userId: string
+
+  @column()
+  declare publishedAt: DateTime | null
+
+  @column()
+  declare status: 'draft' | 'publish'
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @beforeCreate()
+  static generateUUID(model: Post) {
+    model.id = randomUUID()
+  }
+}
---
+++ database/migrations/1725677052559_create_posts_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.string('id')
+      table.string('title').notNullable()
+      table.text('content').notNullable()
+      table.string('image_url').notNullable()
+      table.string('user_id').references('id').inTable('users').onDelete('CASCADE')
+      table.timestamp('published_at').nullable()
+      table.enum('status', ['draft', 'publish']).defaultTo('draft').notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
